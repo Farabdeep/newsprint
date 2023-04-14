090a26d8c69742bef0f6fac335f891658225ab4e
sammyar23 <sammyarora23@gmail.com>
2023-04-14 03:38:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,32 @@ const app = express();
 const port = 3000;
 
 /* Setup database connection */
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST || "localhost",
+  user: "user",
+  password: "password",
+  database: "world",
+});
 
 /* Landing route */
 app.get("/", (req, res) => {
-  res.send("Hello sir!");
+  res.send("Hello world!");
 });
 
+// Sample API route
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+// Returns an array of cities from the database
+app.get("/cities", (req, res) => {
+  db.execute("SELECT * FROM `city`", (err, rows, fields) => {
+    console.log(`/cities: ${rows.length} rows`);
+    return res.send(rows);
+  });
+});
 
 // Run server!
 app.listen(port, () => {
-  console.log(`Server running`);
+  console.log(`Server running on port ${port}`);
 });
