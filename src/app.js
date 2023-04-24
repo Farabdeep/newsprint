64301819693b1973ce5dd34e36260d2295350761
sammy <sammyarora23@gmail.com>
2023-04-24 21:22:46 +0100

    js server file

--- src/app.js
+++ src/app.js
@@ -6,10 +6,3 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    // Set up an array of data
-    var test_data = ['one', 'two', 'three', 'four'];
-    // Send the array through to the template as a variable called data
-    res.render("index", {'title':'My index page', 'heading':'My heading', 'data':test_data});
-   });
